8a8b23f10ea97c8e51a85c077aa5c1d52cd00aad
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-03-17 08:10:25 +0700

    Sắp xếp theo các tiêu chí

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -1,12 +1,22 @@
 import { Request, Response } from "express"
 import Task from "../models/task.model"
 export const index  = async (req: Request, res: Response) => {
+  // Find
   const find ={
     deleted: false
   }
   if(req.query.status){
     find["status"] = req.query.status
   }
+  // End Find
+
+  // Sort
+  const sort = {}
+  if(req.query.sortKey && req.query.sortValue){
+    const sortKey = req.query.sortKey.toLocaleString()
+    sort[sortKey] = req.query.sortValue
+  }
+  // End Sort
   const tasks = await Task.find(find)
   console.log(tasks)
   res.json(tasks)
