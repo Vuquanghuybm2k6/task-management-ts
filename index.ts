9e553c33ce41f6d0707a9967e30996bd0272d436
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-03-17 09:38:12 +0700

    Cài đặt cors

--- index.ts
+++ index.ts
@@ -2,14 +2,22 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import * as database from "./config/database";
 dotenv.config();
-var cors = require('cors')
+
+import cors from "cors"
+
 const app: Express = express();
 const port: number | string= process.env.PORT || 3000;
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
+
+app.use(cors)
+
 database.connect();
+
 import mainV1Routes from "./api/v1/routes/index.route";
 mainV1Routes(app)
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
